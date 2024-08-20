373126eef817c0cb2323446fe48508942cdcdb97
StarSNG25 <starpe.gamer@gmail.com>
2024-08-20 17:32:04 +0700

    Added settings (bugs to be fixed)

---
+++ Starnime/Models/Settings.swift
@@ -0,0 +1,8 @@
+//
+//  Settings.swift
+//  Starnime
+//
+//  Created by Star_SNG on 2024/08/20.
+//
+
+import Foundation
---
+++ Starnime/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Starnime
+//
+//  Created by Star_SNG on 2024/08/20.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
